acb5c58c5ccb36ace585c5a213db003c5dd5cd24
Navin <navin20604@gmail.com>
2025-02-15 00:14:29 -0500

    Building GameCard

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient from "@/services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
@@ -26,7 +27,7 @@ const useGames = () => {
         setError(err.message);
       });
 
-    return()=> controller.abort();
+    return () => controller.abort();
   }, []);
   return { games, error };
 };
