5681d078d5e67d1276e3b2c8fc052647252157ee
Navin <navin20604@gmail.com>
2025-03-06 00:30:36 -0500

    Fetch platforms using react query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -26,7 +26,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
           </Button>
         </MenuTrigger>
         <MenuContent>
-          {data.map((platform) => (
+          {data?.results.map((platform) => (
             <MenuItem
               value={platform.slug}
               key={platform.id}
